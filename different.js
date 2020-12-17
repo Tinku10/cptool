157de4a14f1e842debaca0ab1852f9c31920d7e4
Tinku10 <tinku.kvs@gmail.com>
2020-12-17 14:48:04 +0530

    color change diff

--- different.js
+++ different.js
@@ -16,23 +16,34 @@ module.exports = {
     fs.readdir(dir, (err, files) => {
  
       var passed=0;
-      execSync("g++ -std=c++17 "+fileName+".cpp -o "+fileName);
+      try{
+        execSync("g++ -std=c++17 "+fileName+".cpp -o "+fileName);
+      }catch(err){
+        console.log(err.stdout);
+        console.log(chalk.red(err.stderr));
+        process.exit(1);
+      }
       var len=files.length;
-      for(var i=1;i<=(len-2)/2;i++){ 
-        execSync("./" +fileName+ " < in"+i+" > ans");
+      for(var i=1;i<=(len-1)/2;i++){ 
+        try{
+          execSync("./" +fileName+ " < in"+i+" > ans");
+        }catch(err){
+          console.log(chalk.red(err.stdout));
+          process.exit(1);
+        }
         var ans=fs.readFileSync(dir+"/out"+i,{encoding:'utf8', flag:'r'});
         var actualans=fs.readFileSync(dir+"/ans",{encoding:'utf8', flag:'r'});
-        var diff = Diff.diffTrimmedLines(actualans, ans, {newlineIsToken: true});
+        var diff = Diff.diffWords(actualans, ans, {newlineIsToken: true});
         var cnt = 0;
         diff.forEach((part) => {
           // green for additions, red for deletions
           // grey for common parts
           const color = part.added ? 'brightGreen' :
-            part.removed ? 'red' : 'gray';
+            part.removed ? 'brightRed' : 'gray';
           process.stderr.write(part.value[color]);
           if(part.removed || part.added) cnt++;
         });
-        console.log();
+        // console.log();
         if(cnt==0){
           console.log(chalk.green.bold('Passed Pretest '+ i))
           passed++;
